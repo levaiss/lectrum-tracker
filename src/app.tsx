351bf2aaded38811be88c2844fd84cbd773c7c51
Andrey Prisniak <aprisniak@lectrum.io>
2021-06-28 18:21:11 +0300

    feat(source): initial commit

---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+// Core
+import { FC } from 'react';
+
+// Components
+
+
+// Instruments
+
+
+export const App: FC = () => {
+    return (
+        <main>
+            <h1>Продвинутый виджет погоды</h1>
+        </main>
+    );
+};
+
